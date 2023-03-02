fe88617efb2c0379af338d89b84bd4313f992614
Hein Min Thant <heinminthant1999@gmail.com>
2023-03-02 15:54:38 +0630

    feat: solution for timestamp microservice

--- index.js
+++ index.js
@@ -2,31 +2,54 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+  return res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
 });
 
+app.get("/api/:date", function (req, res) {
+  let date = req.params.date;
+  if (!date) {
+    return res.json({
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString(),
+    });
+  }
 
+  if (!isNaN(Number(date))) {
+    date = Number(date);
+  }
+  if (new Date(date) == "Invalid Date") {
+    return res.json({
+      error: "Invalid Date",
+    });
+  }
+
+  return res.json({
+    unix: new Date(date).getTime(),
+    utc: new Date(date).toUTCString(),
+  });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
